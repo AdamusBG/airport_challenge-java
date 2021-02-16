1cc050ab835bee13802d71878f67ef6c4342293c
Adam Grieve <adamwizzy@hotmail.co.uk>
2021-02-16 18:15:08 +0000

    updated plane class and tests to add dependency to airport class

--- src/main/java/Plane.java
+++ src/main/java/Plane.java
@@ -1,30 +1,30 @@
 public class Plane {
 
-    private String airport;
+    private Airport airport;
 
     // constructor methods, one making a plane in flight and another creating a plane at an airport
 
-    public Plane(String airport) {
+    public Plane(Airport airport) {
         this.airport = airport;
     }
 
     public Plane() {
-        this.airport = "none";
+        this.airport = null;
     }
 
     // methods for asking the plane about its airport
 
-    public String getAirport() {
+    public Airport getAirport() {
         return this.airport;
     }
 
     public boolean atAirport() {
-        return !this.airport.equals("none");
+        return this.airport != null;
     }
 
     // methods for changing the plane's airport
 
-    public void land(String airport) throws Exception {
+    public void land(Airport airport) throws Exception {
         if (this.atAirport()) {
             throw new Exception("This plane is already at an airport!");
         } else { // further errors to throw when airport class implemented
@@ -32,16 +32,16 @@ public class Plane {
         }
     }
 
-    public void takeOff(String airport) throws Exception {
+    public void takeOff(Airport airport) throws Exception {
         if (!this.atAirport()) {
             throw new Exception("This plane cannot take off as it is already in flight!");
         } else if (!this.getAirport().equals(airport)) {
             throw new Exception("The pane can't take off from an airport that it is not at");
         } else { // further errors to throw when airport class implemented
-            this.setAirport("none"); // also include call to airport method here
+            this.setAirport(null); // also include call to airport method here
         }
     }
 
-    private void setAirport(String airport) { this.airport = airport; }
+    private void setAirport(Airport airport) { this.airport = airport; }
 
 }
--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,17 +1,20 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class PlaneTest {
 
     Plane inFlightPlane;
     Plane landedPlane;
+    Airport airportMock1;
+    Airport airportMock2;
 
     @Test
     void getAirport() {
         resetTestObjects();
-        assertEquals("none", inFlightPlane.getAirport(),"plane in flight asked its airport should return \"none\"");
-        assertEquals("airport", landedPlane.getAirport(), "plane at an airport should correctly return its airport when asked");
+        assertEquals(null, inFlightPlane.getAirport(),"plane in flight asked its airport should return null");
+        assertEquals(airportMock1, landedPlane.getAirport(), "plane at an airport should correctly return its airport when asked");
     }
 
     @Test
@@ -25,34 +28,40 @@ class PlaneTest {
     void successful_land() {
         resetTestObjects();
         try {
-            inFlightPlane.land("frenchAirport");
+            inFlightPlane.land(airportMock1);
         } catch (Exception e) {
             System.out.println("if you're seeing this something is really wrong");
         }
-        assertEquals("frenchAirport", inFlightPlane.getAirport(), "an in-flight plane should successfully land and then correctly report its airport");
+        assertEquals(airportMock1, inFlightPlane.getAirport(), "an in-flight plane should successfully land and then correctly report its airport");
     }
 
     @Test
     void unsuccessful_land() {
         resetTestObjects();
-        assertThrows(Exception.class, () -> landedPlane.land("frenchAirport"));
+
+        Exception exception = assertThrows(Exception.class, () -> landedPlane.land(airportMock2));
+
+        String expectedMessage = "This plane is already at an airport!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage), "correct error message is given when telling plane to land when already landed");
     }
 
     @Test
     void successful_takeOff() {
         resetTestObjects();
         try {
-            landedPlane.takeOff("airport");
+            landedPlane.takeOff(airportMock1);
         } catch (Exception e) {
             System.out.println("if you're seeing this something is really wrong");
         }
-        assertEquals("none", inFlightPlane.getAirport(), "a landed plane should take off when told to do so from the correct airport");
+        assertEquals(null, inFlightPlane.getAirport(), "a landed plane should take off when told to do so from the correct airport");
     }
 
     @Test
     void unsuccessfulInFlight_takeOff() {
         resetTestObjects();
-        Exception exception = assertThrows(Exception.class, () -> inFlightPlane.takeOff("airport"));
+        Exception exception = assertThrows(Exception.class, () -> inFlightPlane.takeOff(airportMock1));
 
         String expectedMessage = "This plane cannot take off as it is already in flight!";
         String actualMessage = exception.getMessage();
@@ -63,17 +72,20 @@ class PlaneTest {
     @Test
     void unsuccessfulWrongAirport_takeOff() {
         resetTestObjects();
-        Exception exception = assertThrows(Exception.class, () -> landedPlane.takeOff("frenchAirport"));
+        Exception exception = assertThrows(Exception.class, () -> landedPlane.takeOff(airportMock2));
 
         String expectedMessage = "The pane can't take off from an airport that it is not at";
         String actualMessage = exception.getMessage();
 
-        assertTrue(actualMessage.contains(expectedMessage), "correct error message is given when telling a plane that is already in flight to take off");
+        assertTrue(actualMessage.contains(expectedMessage), "correct error message is given when telling a plane to take off from an airport that it is not landed at");
     }
 
     // prevents the state of the test objects being affected between tests
     private void resetTestObjects() {
+        airportMock1 = mock(Airport.class);
+        airportMock2 = mock(Airport.class);
+
         inFlightPlane = new Plane();
-        landedPlane = new Plane("airport");
+        landedPlane = new Plane(airportMock1);
     }
 }
