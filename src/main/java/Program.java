88ee558d65a932b942f20b5f252307aef03938e5
Adam Grieve <adamwizzy@hotmail.co.uk>
2021-02-17 17:23:05 +0000

    emptied program class

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -3,8 +3,7 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Plane myInFlightPlane = new Plane();
-        Plane myLandedPlane = new Plane("airport");
+
 
     }
 }
